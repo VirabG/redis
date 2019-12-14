40dfef4edd2c76dac2b207501ee7b4b9a87f83a5
Virab Gevorgyan <virab.gevorgyan@yahoo.com>
2019-12-14 21:56:49 +0100

    last changes

--- src/ProtocolHandler.py
+++ src/ProtocolHandler.py
@@ -11,4 +11,20 @@ class ProtocolHandler:
 	
 	def serialize_integer(self, n):
 		return ":" + str(n) + "\r\n"
-		
+	
+	def deserialize_error(self, e):
+		return e[1:-2]
+
+	def serialize_error(self, e):
+		return "-" + e + "\r\n"
+
+	def deserialize_binary(self, s):
+		int_end = s.find("\r")		
+		n_bytes = int(s[1:int_end])
+		data = s[(int_end+4):-2]
+		return n_bytes, data
+
+	def serialize_binary(self, s):
+		n_bytes = len(s)
+		return "$" + str(n_bytes) + "\r\n" + s + "\r\n"
+
--- src/test_ProtocolHandler.py
+++ src/test_ProtocolHandler.py
@@ -6,7 +6,6 @@ def test_string_deserialization():
 	serialized = "+OK\r\n"
 	actual = handler.deserialize_string(serialized)
 	expected = "OK"
-
 	assert(actual == expected)
 	print("test string deserialization passed.")
 
@@ -14,7 +13,6 @@ def test_string_serialization():
 	input = "OK"
 	actual = handler.serialize_string(input)
 	expected = "+OK\r\n"
-
 	assert(actual == expected)
 	print("test string serialization passed.")
 
@@ -29,13 +27,28 @@ def test_integer_serialization():
 	input = 111
 	actual = handler.serialize_integer(input)
 	expected = ":111\r\n"
-
 	assert(actual == expected)
 	print("test integer serialization passed.")
+
+def test_error_deserialization():
+	serialized = "-I am an error\r\n"
+	actual = handler.deserialize_error(serialized)
+	expected = "I am an error"
+	assert (actual == expected)
+	print("test error deserialization passed.")
+
+def test_error_serialization():
+	input = "I am an error"
+	actual = handler.serialize_error(input)
+	expected = "-I am an error\r\n"
+	assert (actual == expected)
+	print("test error serialization passed.")
 
 test_string_deserialization()
 test_string_serialization()
 test_integer_deserialization()
 test_integer_serialization()
+test_error_deserialization()
+test_error_serialization()
 
 
