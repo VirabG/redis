86ac68f21512b3e332671165d503e3727b731fcf
Virab Gevorgyan <virab.gevorgyan@yahoo.com>
2020-05-19 22:06:25 +0200

    Fix python version in tests.

--- redis/tests/test_Client_Server.py
+++ redis/tests/test_Client_Server.py
@@ -3,9 +3,16 @@ import time
 from ..Client import Client
 
 def test_echo_server_with_single_client():
-    p = subprocess.Popen(['python', 'Server.py'])
-    time.sleep(0.1)
+    p = subprocess.Popen(['python3', 'Server.py'])
+    time.sleep(1.1)
     client = Client()
     response = client.set(4, "aaa")
     assert response == "('SET', 4, 'aaa')"
     p.terminate()
+
+"""
+def test_Server_State_Save():
+    p = subprocess.Popen(['python', 'Server.py'])
+    time.sleep(0.1)
+    client = Client()
+"""
